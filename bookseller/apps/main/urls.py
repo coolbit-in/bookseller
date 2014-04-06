78fdfdbd3efaf4aae626dc11fc98b5da10d7e253
coolbit <coolbit.in@gmail.com>
2014-04-06 10:32:08 +0800

    Add account url router

--- bookseller/apps/main/urls.py
+++ bookseller/apps/main/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
 
 from bookseller.apps.main import views
-
+from bookseller.apps.register.views import account
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^account/(\d+)/$', account)
 )
